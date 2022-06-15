8eccea7680173f6c5c41cddb99ea85cdf86aa3c5
jatlasdev <107046052+jatlasdev@users.noreply.github.com>
2022-06-15 11:44:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,74 +1,46 @@
-let namebox = document.getElementById('namebox')
-let addbox = document.getElementById('addbox')
-let add = document.getElementById('add')
-let create = document.getElementById('create')
-let people = [
-    {
-        name: "joe",
-        age: "17"
-    },
-    {
-        name: "jess",
-        age: "20"
-    },
-    {
-        name: "brett",
-        age: "5"
-    }
-]
+let display = document.getElementById('display')
+let submit = document.getElementById('submit')
+let people = []
 
-class Person{
+class Person {
     constructor(name, age) {
         this.name = name;
         this.age = age;
     }
 }
 
-create.addEventListener('click', function (){
-    for(let i=0; i<people.length; i++) {
-        let box = document.createElement('div')
-        box.setAttribute('class', 'box')
-        
-        let namespace = document.createElement('h1')
-        let agespace = document.createElement('h2')
-        namespace.setAttribute('class', 'names')
-        namebox.append(box)
-        namespace.innerHTML=`Name: ${people[i].name}`
-        agespace.innerHTML=`Age: ${people[i].age}`
-        box.append(namespace)
-        box.append(agespace)
-    }
-})
-
-add.addEventListener('click', function() {
-    const newPerson = new Person()
-    let newname = document.getElementById('na')
-    let newage = document.getElementById('ag')
-    newPerson.name = newname.value
-    newPerson.age = newage.value
-    if(!newname.value || !newage.value){
-        return
-    } else {
-        people.push(newPerson)
-        for(let i=0; i<people.length; i++) {
-            document.querySelectorAll('.names').forEach(function(name) {
-                if(name.innerHTML == (people[i].name)) {
-                    console.log('error checking values')
-                } else {
-                    let box = document.createElement('div')
-                    box.setAttribute('class', 'box')
-                    let namespace = document.createElement('h1')
-                    namespace.setAttribute('class', 'names')
-                    let agespace = document.createElement('h2')
-                    addbox.append(box)
-                    namespace.innerHTML=`Name: ${people[i].name}`
-                    agespace.innerHTML=`Age: ${people[i].age}`
-                    box.append(namespace)
-                    box.append(agespace)
-                    }
-                })
-        
-    }
+function isUnique(array, person) {
+    const isNameExists = array.some(item => item.name === person.name)
+    if (!isNameExists) array.push(person)
+    return array
 }
-    
+
+
+submit.addEventListener('click', function() {
+    let inName = document.getElementById('inName')
+    let inAge = document.getElementById('inAge')
+    const newPerson = new Person()
+    newPerson.name = inName.value
+    newPerson.age = inAge.value
+    if(!inName.value || !inAge.value) {
+        alert('Missing')
+    } else {
+        const newPeopleList = isUnique(people, newPerson)
+        display.innerHTML = ""
+        newPeopleList.map(person => {
+            let box = document.createElement('div')
+                box.setAttribute('class', 'box')
+                let namespace = document.createElement('h1')
+                namespace.setAttribute('class', 'names')
+                let agespace = document.createElement('h2')
+                display.append(box)
+                namespace.innerHTML=`Name: ${person.name}`
+                agespace.innerHTML=`Age: ${person.age}`
+                box.append(namespace)
+                box.append(agespace)
+                inName.value=""
+                inAge.value=""
+        })
+        
+    }
 })
